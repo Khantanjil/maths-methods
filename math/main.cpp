123e474aea1570a991c5b6a9e77cb4ec177a39bf
Tanjil <tanjilkhan.sh@gmail.com>
2020-05-15 14:37:55 +0100

    Add files via upload

---
+++ math/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+
+    double myNum = 1.241;
+    double myNum1 = 1.240;
+    cout << max(myNum, myNum1) << endl;
+
+    double number = 50.12;
+    double number1 = 50.11;
+    cout << min(number, number1) << endl;
+
+    int num = 25;
+    cout << sqrt(num) << endl; // Square root
+
+    float num1 = 25.651;
+    cout << round(num1) << endl;
+
+    int numb = 2092;
+    int numb1 = 2094;
